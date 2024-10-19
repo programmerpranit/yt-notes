260de8b1d79d89e3a8ef72651f286b95dfbd4370
Pranit Patil <dev.pranitpatil@gmail.com>
2024-10-19 22:53:51 +0530

    add all

---
+++ src/app/HomePage.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import { getYoutubeSubtitles, summerizeTranscript } from "./actions";
+import { marked } from "marked";
+import jsPDF from "jspdf";
+
+const HomePage = () => {
+    const [url, setUrl] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [loadingText, setLoadingText] = useState("Loading...");
+    const [summary, setSummary] = useState("");
+
+    const [error, setError] = useState("");
+
+    const handleGetSubtitles = async () => {
+        try {
+            setLoading(true);
+            setLoadingText("Fetching Youtube video...");
+            const videoId = url.split("v=")[1].split("&")[0];
+
+            if (!videoId) {
+                console.log("No video ID found");
+                alert("No video ID found");
+                return null;
+            }
+
+            const transcript = await getYoutubeSubtitles(videoId);
+            console.log(transcript);
+            setLoadingText("Generating Notes...");
+            const rawSummary = await summerizeTranscript(transcript);
+            const summary = rawSummary.replaceAll("**", "");
+            console.log(summary);
+            setSummary(summary || "");
+            setLoadingText("Generating PDF...");
+            const doc = new jsPDF();
+            const lines = doc.splitTextToSize(summary || "", 200); // Wrap text
+            doc.setFontSize(14);
+            let y = 20;
+            lines.forEach((line: string) => {
+                if (y > 280) { // Check if the current page is full
+                    doc.addPage(); // Add a new page if the current page is full
+                    y = 20; // Reset y position for the new page
+                }
+                doc.text(line, 20, y);
+                y += 10; // Move y position for the next line
+            });
+            doc.save('yt.pdf');
+            setLoadingText("Loading...");
+            setLoading(false);
+
+        } catch (error) {
+            console.log(error);
+            setLoadingText("Error");
+            setError(`Got some error: ${error?.toString()}`);
+            setLoading(false);
+        }
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-5 h-screen">
+            <input placeholder="Enter YouTube URL" className="p-2 rounded-md border-2 w-1/2 border-gray-300 outline-none" type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+            <button disabled={loading} className="p-2 rounded-md disabled:bg-gray-400 bg-blue-500 text-white" onClick={handleGetSubtitles}>{loading ? <div className="h-5 w-5 rounded-full border-white border-b-black border-t-2 animate-spin"></div> : "Get Notes"}</button>
+
+            {loading && <p className="text-lg font-bold justify-center items-center flex">{loadingText}</p>}
+
+            {error && <p className="text-lg text-red-500 font-bold justify-center items-center flex">{error}</p>}
+
+        </div>
+    )
+}
+
+export default HomePage
---
+++ src/app/actions.ts
@@ -0,0 +1,23 @@
+"use server";
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+import { YoutubeTranscript, type TranscriptResponse } from "youtube-transcript";
+
+export async function getYoutubeSubtitles(videoId: string) {
+  const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+  return transcript;
+}
+
+export async function summerizeTranscript(transcript: TranscriptResponse[]) {
+  // open ai api call
+
+  const response = await generateText({
+    model: openai("gpt-4o"),
+    prompt: `You are a helpful assistant that summarizes youtube transcripts. In form of educative notes to refer them later. Focus on educative content Write summary in very understandable way. Make it more informative than making it short. Dont make it look like it is summary of a youtube video just get the content. Make it look beautiful. Here is the transcript: ${transcript
+      .map((t) => t.text)
+      .join(" ")}`,
+  });
+
+  console.log(response.text);
+  return response.text;
+}
